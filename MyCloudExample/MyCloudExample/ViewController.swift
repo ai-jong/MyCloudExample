b2dab64a900a2eb7ab34743790f913f845f6a7c9
ai-jong <ai.jon.blaine@gmail.com>
2024-09-27 21:02:18 -0700

    Add files via upload

---
+++ MyCloudExample/MyCloudExample/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  MyCloudExample
+//
+//  Created by John Blaine on 9/26/24.
+//
+
+import UIKit
+import MyCloud
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.myCloudExample()
+    }
+    
+    func myCloudExample() {
+        let myCloud = MyCloud()
+        let data = ["name" : "jon" ]
+        
+        myCloud.write("folder", merge: true, data: data) { value in
+        }
+        
+        myCloud.read("folder") { (value : [AnyHashable : Any]) in
+            print( "\(value)")
+        }
+        
+        myCloud.watch("folder") { (value : [AnyHashable : Any]) in
+            print( "\(value)")
+        }
+    }
+    
+
+}
+
